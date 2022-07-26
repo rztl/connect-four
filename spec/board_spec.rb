de93429bfe7ba76f77c13df3868b43040aaff8c9
matristle <97355883+matristle@users.noreply.github.com>
2022-07-27 01:27:12 +0300

    Add Board#drop examples

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,32 +1,67 @@
-require './lib/board'
+require_relative '../lib/board'
 
 describe Board do
-  it 'updates as oooo on last row' do
-    board_instance = Board.new
-    board_instance.board = 
-		[
-      ['' , '' , '' , '' , '', '', ''],
-      ['' , '' , '' , '' , '', '', ''],
-      ['' , '' , '' , '' , '', '', ''],
-      ['' , '' , '' , '' , '', '', ''],
-      ['' , '' , '' , '' , '', '', ''],
-      ['o', 'o', 'o', '' , '', '', '']
-    ]
-    piece = 'o'
-    coordinates = [0, 3]
+  describe '#drop' do
+		context 'when there are no pieces on the chosen column' do
+			it 'drops the piece to the bottom of the board' do
+				board_array = 
+				[
+					['' , '' , '' , '' , '', '', ''],
+					['' , '' , '' , '' , '', '', ''],
+					['' , '' , '' , '' , '', '', ''],
+					['' , '' , '' , '' , '', '', ''],
+					['' , '' , '' , '' , '', '', ''],
+					['o', 'o', '' , '' , '', '', '']
+				]
+				expected_board_array =
+				[
+					['' , '' , '' , '' , '', '', ''],
+					['' , '' , '' , '' , '', '', ''],
+					['' , '' , '' , '' , '', '', ''],
+					['' , '' , '' , '' , '', '', ''],
+					['' , '' , '' , '' , '', '', ''],
+					['o', 'o', 'o', '' , '', '', '']
+				] 
+				board = described_class.new(board_array)
+	
+				board.drop('o', 2)
+	
+				expect(board.board_array).to eq(expected_board_array)
+			end
+		end
 
-    board_instance.drop(piece, coordinates)
-    updated_board = board_instance.board
+		context 'when there are inserted pieces on the chosen column' do
+			it 'drops the piece above the top pre-inserted piece' do
+				board_array = 
+				[
+					['' , '' , '' , '' , '', '', ''],
+					['' , '' , '' , '' , '', '', ''],
+					['x' , '' , '' , '' , '', '', ''],
+					['x' , '' , '' , '' , '', '', ''],
+					['o', '' , '' , '' , '', '', ''],
+					['o', '' , '' , '' , '', '', '']
+				]
+				# expected_board_array =
+				# [
+				# 	['' , '' , '' , '' , '', '', ''],
+				# 	['' , '' , '' , '' , '', '', ''],
+				# 	['' , '' , '' , '' , '', '', ''],
+				# 	['x' , '' , '' , '' , '', '', ''],
+				# 	['o', '' , '' , '' , '', '', ''],
+				# 	['o', '' , '' , '' , '', '', '']
+				# ]
 
-    expect(updated_board).to eq(
-      [
-        ['' , '' , '' , '' , '', '', ''],
-        ['' , '' , '' , '' , '', '', ''],
-        ['' , '' , '' , '' , '', '', ''],
-        ['' , '' , '' , '' , '', '', ''],
-        ['' , '' , '' , '' , '', '', ''],
-        ['o', 'o', 'o', 'o', '', '', '']
-      ] 
-    )
-  end
+				expected_column = ['', 'x', 'x', 'x', 'o', 'o']
+				board = described_class.new(board_array)
+	
+				board.drop('x', 0)
+	
+				expect(board.column).to eq(expected_column)
+			end
+		end
+	end
+
+	it 'updates as oooo on last row' 
+	it 'returns winning message' 
+	it 'returns losing message'
 end

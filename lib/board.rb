48d6354c87ea89da88872e84c38a0d9039527300
matristle <97355883+matristle@users.noreply.github.com>
2022-07-27 01:28:19 +0300

    Add Board#drop functionality w.r.t. examples

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,25 @@
-class ConnectFour
-  attr_accessor :board
+class Board 
+  attr_accessor :board_array, :column
+  
+  def initialize(board_array)
+    @board_array  = board_array
+    @column = nil
+  end
 
-  def drop(piece, coordinates)
-    row_number    = board.size - 1
-    column_number = coordinates[1]
+  def drop(piece, column_number)
+    # column_number = column_position # potential API-internal conversion
+    empty_string = ''
+    
+    self.column = board_array.collect { |row| row[column_number] }
+    pieces_in_column = column.select { |slot| slot unless slot == empty_string }
 
-    board[row_number][column_number] = piece
+    if column.all?(empty_string)
+      row_number = board_array.size - 1 
+    else
+      row_number = (column.size - pieces_in_column.size) - 1
+    end
+
+    board_array[row_number][column_number] = piece    
+    column[row_number] = piece    
   end
 end
